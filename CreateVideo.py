286067c4fec22f670a3c8aef02ecb433b93f1495
sanillandge <82667082+sanillandge@users.noreply.github.com>
2022-08-02 20:10:53 +0530

    Add files via upload

---
+++ CreateVideo.py
@@ -0,0 +1,34 @@
+import os
+from tkinter import Frame
+import cv2
+
+
+path = "Images"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+        print(file_name)
+               
+        images.append(file_name)
+        
+print(len(images))
+count = len(images)
+frame = cv2.imread(images[0])
+height,width,channels =frame.shape
+size = (width,height)
+out = cv2.VideoWriter("SUN.mp4",cv2.VideoWriter_fourcc( *'DIVX'),20,size)
+for i in range(count-1,0,-1):
+    frame=cv2.imread(images[i])
+    out.write(frame)
+
+out.release()
+print("done.")
+
+
